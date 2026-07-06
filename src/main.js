22b6c4bd17b8871b09808e2432e524a85fb34cee
MaulanaaEkaa <MaulanaOioi827@gmail.com>
2026-07-06 08:31:29 +0700

    install dependency untuk font awesome

--- src/main.js
+++ src/main.js
@@ -10,6 +10,16 @@ import { useUserStore } from "@/stores/user";
 import App from "./App.vue";
 import router from "./router";
 
+// Font Awesome Import
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(fas, far, fab)
+
 const app = createApp(App);
 const pinia = createPinia()
 
@@ -24,7 +34,7 @@ userStore
   .listenToAuthState()
   .then(() => {
     app.use(router)
-    // app.component('font-awesome-icon', FontAwesomeIcon)
+    app.component('font-awesome-icon', FontAwesomeIcon)
     app.mount('#app')
   })
   .catch((error) => {
